f103cfcb8b0b005a47d804338dd87de8285995fa
thehancode <the.hancode1@gmail.com>
2024-07-14 21:49:22 -0500

    astro config site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import icon from "astro-icon"
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
+  site: 'https://thehansoft.com',
+  base: '/',
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
   }), compress()],
