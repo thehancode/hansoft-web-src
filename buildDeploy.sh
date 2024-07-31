d8081f9e297adfa511679bfeee3988f0f15d7067
thehancode <the.hancode1@gmail.com>
2024-07-31 15:19:02 -0500

    title colors

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -5,7 +5,7 @@ git checkout HEAD -- dist .gitignore
 mv dist/* dist/.* .
 rm -rf  dist
 git add .
-git commit -m "Update gh-pages with latest build"
+git commit -m "latest build"
 git push origin temp-gh-pages:gh-pages --force
 git checkout main
 git branch -D temp-gh-pages
