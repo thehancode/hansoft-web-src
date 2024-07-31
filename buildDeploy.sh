3de9e8117b238156126ef4050f05de2ca5ed0a08
thehancode <the.hancode1@gmail.com>
2024-07-31 15:15:05 -0500

    script for deploy

--- buildDeploy.sh
+++ buildDeploy.sh
@@ -1,8 +1,9 @@
+npm run build
 git checkout -b temp-gh-pages
 git rm -rf .
 git checkout HEAD -- dist .gitignore
-mv dist/* .
-rmdir dist
+mv dist/* dist/.* .
+rm -rf  dist
 git add .
 git commit -m "Update gh-pages with latest build"
 git push origin temp-gh-pages:gh-pages --force
